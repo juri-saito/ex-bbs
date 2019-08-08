c0d988f7b8dd733950f89c762953bcb735a33e59
jurisaito <juri.saito@rakus-partners.co.jp>
2019-08-08 15:12:53 +0900

    Articleリポ時とに記事一覧を取得するメソッド追加。 Articleコントローラに掲示板画面を表示するメソッドを追加。

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 掲示板関連機能の処理の制御を行うコントローラ
@@ -8,6 +9,16 @@ import org.springframework.stereotype.Controller;
  *
  */
 @Controller
+@RequestMapping("/bbs")
 public class ArticleController {
+	
+	/**
+	 * 掲示板画面を表示する
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("/")
+	public String index() {
+		return "bbs";
+	}
 
 }
--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -1,6 +1,13 @@
 package com.example.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
 
 /**
  * articlesテーブルを操作するリポジトリ
@@ -9,5 +16,29 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class ArticleRepository {
+	
+	@Autowired
+	private  NamedParameterJdbcTemplate template;
+	
+	/**
+	 * Articleオブジェクトを生成するローマッパー
+	 */
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
+		Article article = new Article();
+		article.setId(rs.getInt("id"));
+		article.setName(rs.getString("name"));;
+		article.setContent(rs.getString("content"));
+		return article;
+	};
+	
+	/**
+	 * 記事一覧情報を投稿順で取得する
+	 * @return 記事一覧
+	 */
+	public List<Article> findAll(){
+		String sql = "";
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+		return articleList;
+	}
 
 }
